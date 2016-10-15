4c7b2c02e97a311e3484c9569c6637ce36b29ee7
Tallone <seagullsay3@yahoo.com>
2016-10-15 19:49:13 +0800

    fix encoding error

--- crawler.py
+++ crawler.py
@@ -5,45 +5,51 @@ import json
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 
-r = requests.get("http://www.ishadowsocks.net/")
+r = requests.get("http://www.ishadowsocks.org/")
+r.encoding = 'utf-8'
 soup = BeautifulSoup(r.text, "html.parser")
 pw = soup.find_all(text=re.compile('密码'))
 server = soup.find_all(text=re.compile('服务器'))
 port = soup.find_all(text=re.compile('端口'))
 status = soup.find_all('font')
 status = [status[0].text, status[2].text, status[4].text]
+
+
 def f(s):
-	return s.split(':')[1]
-pw = list(map(f, [pw[0],pw[2],pw[4]]))
-server = list(map(f, [server[0],server[1],server[2]]))
-port = list(map(f, [port[0],port[1],port[2]]))
+    return s.split(':')[1]
+
+
+pw = list(map(f, [pw[0], pw[2], pw[4]]))
+server = list(map(f, [server[0], server[1], server[2]]))
+port = list(map(f, [port[0], port[1], port[2]]))
 l = []
 for i in range(3):
-	if status[i] == "正常":
-		d = {"method": "aes-256-cfb", "server": server[i], "password": pw[i], "remarks": "", "server_port": port[i]}
-		l.append(d)
-	else:
-		continue
+    if status[i] == "正常":
+        d = {"method": "aes-256-cfb", "server": server[i], "password": pw[i], "remarks": "", "server_port": port[i]}
+        l.append(d)
+    else:
+        continue
 
 info = PrettyTable(["服务器", "端口", "密码", "状态"])
 for i in range(3):
-	info.add_row([server[i], port[i], pw[i], status[i]])
+    info.add_row([server[i], port[i], pw[i], status[i]])
 print(info)
 
 
 def get_pw(j):
-	for x in range(len(j)):
-		yield j[x]["server"],j[x]["password"]
+    for x in range(len(j)):
+        yield j[x]["server"], j[x]["password"]
+
+
 with open("config.json", "r") as c:
-	p = json.load(c)
-	p = p["gui-config-path"]
-	with open(p, "r") as s:
-		j = json.load(s)
-		j["configs"] = l
-		with open(p, "w") as n_c:
-			try:
-				json.dump(j, n_c)
-				print("写入成功")
-			except:
-				print("写入失败")
-			
+    p = json.load(c)
+    p = p["gui-config-path"]
+    with open(p, "r") as s:
+        j = json.load(s)
+        j["configs"] = l
+        with open(p, "w") as n_c:
+            try:
+                json.dump(j, n_c)
+                print("写入成功")
+            except:
+                print("写入失败")
