e4413eea0fbca5aca956698f20dd42d3d93edb98
Tallone <shitailong@xuxu.in>
2017-05-02 11:02:04 +0800

    update iss url

--- crawler.py
+++ crawler.py
@@ -5,14 +5,12 @@ import json
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 
-r = requests.get("http://www.ishadowsocks.org/")
+r = requests.get("https://b.ishadow.tech/")
 r.encoding = 'utf-8'
 soup = BeautifulSoup(r.text, "html.parser")
 pw = soup.find_all(text=re.compile('密码'))
 server = soup.find_all(text=re.compile('服务器'))
 port = soup.find_all(text=re.compile('端口'))
-status = soup.find_all('font')
-status = [status[0].text, status[2].text, status[4].text]
 
 
 def f(s):
@@ -24,15 +22,12 @@ server = list(map(f, [server[0], server[1], server[2]]))
 port = list(map(f, [port[0], port[1], port[2]]))
 l = []
 for i in range(3):
-    if status[i] == "正常":
-        d = {"method": "aes-256-cfb", "server": server[i], "password": pw[i], "remarks": "", "server_port": port[i]}
-        l.append(d)
-    else:
-        continue
+    d = {"method": "aes-256-cfb", "server": server[i], "password": pw[i], "remarks": "", "server_port": port[i]}
+    l.append(d)
 
-info = PrettyTable(["服务器", "端口", "密码", "状态"])
+info = PrettyTable(["服务器", "端口", "密码"])
 for i in range(3):
-    info.add_row([server[i], port[i], pw[i], status[i]])
+    info.add_row([server[i], port[i], pw[i]])
 print(info)
 
 
